ba345cd9c61f864ea893a1fc7565a059a76aa529
Roman Cold <colddevelop@gmail.com>
2022-01-13 12:25:21 +0300

    Схема для сделок

--- types/deals.d.ts
+++ types/deals.d.ts
@@ -7,18 +7,30 @@ declare module 'crm-core' {
       /**
        * Схема хранения сделки
        */
+
+      export type Status = "Новая" | "Подготовка документов" | "Счет на предоплату" | "В работе" | "Финальный счет"
+
       export interface Schema {
+        _id?: string;
         name: string;
+        author: string;
         sum: number | 0;
         fuelType: string | null;
-        owner: any;
-        permissions: any;
-        ownership: string | Company.Ownership;
+        owner: string;
+        permissions: string;
+        ownership: string | Core.Company.Ownership;
         dealCreated: Date;
         fuelAmount: number | 0;
-        tags: string | null;
+        tags: Array<string> | [];
         fullname: string | null;
         source: string | null;
+        status: string | Core.Deals.Status;
+        history: Map<string, any> | Core.Deals.History;
+      }
+
+      export interface History {
+        comments: Array<string>;
+        whoChange: string;
       }
     }
 
