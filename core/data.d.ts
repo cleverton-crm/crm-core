d15a4b853d036f6ace49d459736fdd9dca7f3829
Roman Cold <colddevelop@gmail.com>
2022-01-14 13:18:56 +0300

    Фикс

--- core/data.d.ts
+++ core/data.d.ts
@@ -1,8 +1,9 @@
 /// <reference types="types/cars" />
 /// <reference types="types/settings" />
 /// <reference types="types/deals" />
+/// <reference types="types/leads" />
 /// <reference types="types/company" />
 /// <reference types="types/clients" />
 /// <reference types="types/personal" />
 /// <reference types="types/profiles" />
-declare module 'crm-core' { }
+declare module 'crm-core' {}
