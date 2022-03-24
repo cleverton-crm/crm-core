38587232b34e4e7f459a1ec8471d0645b67c6417
Roman Cold <colddevelop@gmail.com>
2022-03-24 12:12:33 +0300

    Park interfaces

---
+++ types/park.d.ts
@@ -0,0 +1,33 @@
+declare module 'crm-core' {
+  export namespace Core {
+    export namespace ParkCompany {
+      interface FuelInfo {
+        id: string
+        name: string // Тип топлива
+        capacity: number // Емкость
+        consumption: number // Потребление
+      }
+
+      export interface ParkObject {
+        id: string
+        name: string // Название
+        address?: string // Адрес
+        havePump?: boolean // Наличе насоса
+        distance?: string // Дистанция по бездорожью
+        fuels: IFuelInfo[] // Виды топлива
+        resultCapacity: number // Емкость по объекту (складывается из всех видов топлива)
+        resultConsumption: number // Потребление по объекту (складывается из всех видов топлива)
+        ownerId: string // Ответсвенный объекта парка (тот кто создал)
+        ownerFirst: string // Имя
+        ownerLast: string // Фамилия
+      }
+
+      export interface Park {
+        companyId: string // id компании
+        objects: IParkObject[] // Объекты парка
+        allCapacity: number // Общая емкость
+        allConsumption: number // Общее потребление
+      }
+    }
+  }
+}
