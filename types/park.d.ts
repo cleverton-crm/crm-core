f90f1824b2fcc68577d5a6dd766fb4b9c583d608
Roman Cold <colddevelop@gmail.com>
2022-03-25 11:19:14 +0300

    fix

--- types/park.d.ts
+++ types/park.d.ts
@@ -3,14 +3,14 @@ declare module 'crm-core' {
     export namespace Company {
       export namespace ParkCompany {
         interface FuelInfo {
-          id: number
+          id: string
           name: string // Тип топлива
           capacity: number // Емкость
           consumption: number // Потребление
         }
 
         export interface ParkObject {
-          id: number
+          id: string
           name: string // Название
           address?: string // Адрес
           havePump?: boolean // Наличе насоса
