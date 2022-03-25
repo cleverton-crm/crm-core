3cd890c792e33445223a4f81af8cb4933dba2353
Roman Cold <colddevelop@gmail.com>
2022-03-25 13:23:14 +0300

    Schema interfaces for ParkCompany

--- types/park.d.ts
+++ types/park.d.ts
@@ -2,7 +2,7 @@ declare module 'crm-core' {
   export namespace Core {
     export namespace Company {
       export namespace ParkCompany {
-        interface FuelInfo {
+        export interface FuelInfo {
           id: number
           name: string // Тип топлива
           capacity: number // Емкость
@@ -18,6 +18,28 @@ declare module 'crm-core' {
           fuels: FuelInfo[] // Виды топлива
           resultCapacity: number // Емкость по объекту (складывается из всех видов топлива)
           resultConsumption: number // Потребление по объекту (складывается из всех видов топлива)
+        }
+
+        export interface ParkSchema {
+          company: string // id компании
+          author: string
+          owner: string
+          object: string | 'park';
+          stores: Record<string, any> // Объекты парка
+        }
+
+        export interface FuelInfoSchema {
+          name: string // Тип топлива
+          capacity: number // Емкость
+          consumption: number // Потребление
+        }
+
+        export interface ParkObjectSchema {
+          name: string // Название
+          address?: string // Адрес
+          havePump?: boolean // Наличе насоса
+          distance?: string // Дистанция по бездорожью
+          fuels: Record<string, any> // Виды топлива
         }
 
         export interface Park {
